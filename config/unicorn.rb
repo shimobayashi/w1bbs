09e28aaa6d7d11134489527e52339620e82342d6
Akimasa Shimobayashi <akimasa@shimobayashi.name>
2012-05-11 17:01:30 +0900

    fix for production

---
+++ config/unicorn.rb
@@ -0,0 +1,33 @@
+# copied from http://d.hatena.ne.jp/milk1000cc/20100804/1280893810
+# ワーカーの数
+worker_processes 2
+
+# capistrano 用に RAILS_ROOT を指定
+working_directory "/home/akimasa/w1bbs"
+
+# ソケット
+listen '/tmp/w1bbs_unicorn.sock'
+
+# ログ
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+# ダウンタイムなくす
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      # SIGTTOU だと worker_processes が多いときおかしい気がする
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
