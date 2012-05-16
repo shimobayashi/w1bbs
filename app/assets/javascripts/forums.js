59954cc33b2efeb15e2191db6ed2e83d27eafea5
Akimasa Shimobayashi <akimasa@shimobayashi.name>
2012-05-16 19:51:26 +0900

    become ajax post

---
+++ app/assets/javascripts/forums.js
@@ -0,0 +1,27 @@
+$(function() {
+  $('#new_message')
+    .bind('ajax:success', function(event, data, status, xhr) {
+      var form = $(this);
+      form.find('.validation-error').empty();
+      form.find('#message_body').val('');
+      var dl = form.parent().find('dl');
+      dl.append(xhr.responseText);
+    })
+    .bind('ajax:error', function(event, xhr, status, error) {
+      var form = $(this);
+      var errors, errorText;
+
+      try {
+        errors = $.parseJSON(xhr.responseText);
+      } catch (e) {
+        errors = {message: "Something wrong!"};
+      }
+
+      errorText = $('<ul/>');
+      for (error in errors) {
+        errorText.append($('<li/>').text(error + ':' + errors[error]));
+      }
+
+      form.find('.validation-error').html(errorText);
+    });
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,30 @@
 class MessagesController < ApplicationController
+  respond_to :html, :json
+
   def create
     @forum = Forum.find(params[:forum_id])
     @message = @forum.messages.build(params[:message])
     @message.name = session[:nickname]
     if @message.save
-      redirect_to "/forums/#{@forum.id}/#{@message.position}-"
+      respond_with do |format|
+        format.html do
+          if request.xhr?
+            render @message
+          else
+            redirect_to "/forums/#{@forum.id}/#{@message.position}-"
+          end
+        end
+      end
+    else
+      respond_with do |format|
+        format.html do
+          if request.xhr?
+            render :json => @message.errors, :status => :unprocessable_entity
+          else
+            render
+          end
+        end
+      end
     end
   end
 end
