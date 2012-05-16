bf23df3ce0faf7441db0ca65ae517ba4faead15c
Akimasa Shimobayashi <akimasa@shimobayashi.name>
2012-05-16 19:56:32 +0900

    fix bind to live

--- app/assets/javascripts/forums.js
+++ app/assets/javascripts/forums.js
@@ -1,13 +1,13 @@
 $(function() {
   $('#new_message')
-    .bind('ajax:success', function(event, data, status, xhr) {
+    .live('ajax:success', function(event, data, status, xhr) {
       var form = $(this);
       form.find('.validation-error').empty();
       form.find('#message_body').val('');
       var dl = form.parent().find('dl');
       dl.append(xhr.responseText);
     })
-    .bind('ajax:error', function(event, xhr, status, error) {
+    .live('ajax:error', function(event, xhr, status, error) {
       var form = $(this);
       var errors, errorText;
 
