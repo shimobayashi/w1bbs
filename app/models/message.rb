f99fee01fdcfc45602da437958fb8603555a21b7
Akimasa Shimobayashi <akimasa@shimobayashi.name>
2012-05-18 20:15:23 +0900

    missspell

--- app/models/message.rb
+++ app/models/message.rb
@@ -47,7 +47,7 @@ class Message < ActiveRecord::Base
       body.gsub!(/\n/, '<br />')
       URI.extract(body, ['http', 'https']).each do |url|
         raw_url = CGI::unescapeHTML(url)
-        body.gsub!(url, "<a href='#{URI.escape(raw_url, /[^-_.!~*()a-zA-Z\d;\/?:@&=+$,\[\]%]/n)}' target='_blank'>#{url}</a>") # Escape include single quote and no escape persent
+        body.gsub!(url, "<a href='#{URI.escape(raw_url, /[^-_.!~*()a-zA-Z\d;\/?:@&=+$,\[\]%]/n)}' target='_blank'>#{url}</a>") # Escape include single quote and no escape percent
       end
       body.gsub!(/&gt;&gt;([0-9]+)?(-([0-9]+))?/) do
         "<a href='/forums/#{self.forum.id}/#{$1}#{$2}'>#{$&}</a>"
